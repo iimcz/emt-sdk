4a41ff19120d758a4a1569c9112c50b8af7ebee8
virusek20 <gmoder.virusek20@gmail.com>
2021-06-11 11:10:15 +0200

    Add package loading and validation

---
+++ emt-sdk-test/PackageLoaderTest.cs
@@ -0,0 +1,38 @@
+using emt_sdk.ScenePackages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace emt_sdk_test
+{
+    [TestClass]
+    public class PackageLoaderTest
+    {
+        [TestMethod]
+        public void TestLoadValid()
+        {
+            PackageLoader loader = new PackageLoader();
+            loader.LoadPackage(File.OpenRead("ScenePackages/package-desc-example.json"));
+        }
+
+        [TestMethod]
+        public void TestLoadInvalid()
+        {
+            PackageLoader loader = new PackageLoader();
+            Assert.ThrowsException<InvalidDataException>(() =>
+            {
+                loader.LoadPackage(File.OpenRead("ScenePackages/package-desc-example-invalid.json"));
+            });
+        }
+
+        [TestMethod]
+        public void TestLoadNull()
+        {
+            PackageLoader loader = new PackageLoader();
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                loader.LoadPackage(null);
+            });
+        }
+    }
+}
