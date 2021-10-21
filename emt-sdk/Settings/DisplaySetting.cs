ba7c383215e538915d9892500c9e95c2eacf801d
mandamic <mandamic@fit.cvut.cz>
2021-10-21 11:31:59 +0200

    Add crossover display setting

--- emt-sdk/Settings/DisplaySetting.cs
+++ emt-sdk/Settings/DisplaySetting.cs
@@ -5,5 +5,6 @@
         public int DisplayId { get; set; }
         public ColorSetting Color { get; set; } = new ColorSetting();
         public SkewSetting Skew { get; set; } = new SkewSetting();
+        public float CrossOver { get; set; }
     }
 }
