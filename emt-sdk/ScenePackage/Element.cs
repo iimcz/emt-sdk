4f1f1c7108b298d0898b378fbe8239a878c2f3a0
Michal Manda <mandamic@fit.cvut.cz>
2022-10-10 01:00:46 +0200

    Add element size parsing

---
+++ emt-sdk/ScenePackage/Element.cs
@@ -0,0 +1,25 @@
+﻿using emt_sdk.ScenePackage;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace emt_sdk.Generated.ScenePackage
+{
+    public partial class Element
+    {
+        private int[] ViewportSizes => Regex.Match(ViewportTransform, @"(\d+)x(\d+)([+-]\d+)([+-]\d+)")
+            .Groups
+            .Cast<Capture>()
+            .Skip(1)
+            .Select(x => int.Parse(x.Value))
+            .ToArray();
+
+        public Viewport Viewport
+        {
+            get
+            {
+                var sizes = ViewportSizes;
+                return new Viewport(sizes[0], sizes[1], sizes[2], sizes[3]);
+            }
+        }
+    }
+}
---
+++ emt-sdk/ScenePackage/Viewport.cs
@@ -0,0 +1,19 @@
+﻿namespace emt_sdk.ScenePackage
+{
+    public class Viewport
+    {
+        public Viewport(int width, int height, int x, int y)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
