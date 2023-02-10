641c6d805322ee2e15b90acb9dc6d788792404b7
Michal Manda <mandamic@fit.cvut.cz>
2023-02-10 13:29:43 +0100

    Add basic diagnostic service
    Implementations are runtime specific, not included

---
+++ emt-sdk/Diagnostics/DiagnosticService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Naki3D.Common.Protocol;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace emt_sdk.Diagnostics
+{
+    public class DiagnosticService : Naki3D.Common.Protocol.DiagnosticService.DiagnosticServiceBase
+    {
+        private readonly IDiagnosticLogProvider _log;
+        //private readonly ISystemServiceManager _systemService;
+
+        public DiagnosticService(IDiagnosticLogProvider log)//, ISystemServiceManager systemService)
+        {
+            _log = log;
+            //_systemService = systemService;
+        }
+
+        public override Task<LogsResponse> GetLogs(LogsRequest request, ServerCallContext context)
+        {
+            var logText = _log.ReadMainLog();
+            var requestedLines = logText.Reverse().Take(request.Lines).Reverse();
+
+            var response = new LogsResponse();
+            response.LogLine.AddRange(requestedLines);
+
+            return Task.FromResult(response);
+        }
+
+        public override Task<ServiceStatusResponse> GetServiceStatus(ServiceStatusRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
