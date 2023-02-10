f801aad1715166032c0876b5d0dbae9365d10da7
Michal Manda <mandamic@fit.cvut.cz>
2023-02-10 13:28:39 +0100

    Add connection service

---
+++ emt-sdk/Communication/Exhibit/ConnectionService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using Naki3D.Common.Protocol;
+using System.Threading.Tasks;
+
+namespace emt_sdk.Communication.Exhibit
+{
+    public class ConnectionService : Naki3D.Common.Protocol.ConnectionService.ConnectionServiceBase
+    {
+        public override Task<PingResponse> Ping(PingRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new PingResponse
+            {
+                Echo = request.Msg
+            });
+        }
+    }
+}
