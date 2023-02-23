3c033c1d3e440c92f9dbafb08e88b7f975bd7fd3
Ondřej Slabý <kron258@gmail.com>
2023-02-23 21:36:14 +0100

    Fix UDP broadcast not sending packets

--- emt-sdk/Communication/Discovery/UDPDiscoveryService.cs
+++ emt-sdk/Communication/Discovery/UDPDiscoveryService.cs
@@ -6,17 +6,20 @@ using emt_sdk.Settings;
 using emt_sdk.Settings.EMT;
 using Naki3D.Common.Protocol;
 using Google.Protobuf;
+using NLog;
 
 namespace emt_sdk.Communication.Discovery
 {
     public class UDPDiscoveryService : IDiscoveryService
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private const int DISCOVERY_PORT = 37514;
         private const int DISCOVERY_PERIOD = 15_000; // In milliseconds
 
         private readonly UdpClient _udpClient = new UdpClient();
         private readonly byte[] _beacon;
         private readonly Timer _broadcastTimer = new Timer(DISCOVERY_PERIOD);
+        private readonly IPEndPoint _broadcastEndPoint = new IPEndPoint(IPAddress.Broadcast, DISCOVERY_PORT);
 
         private readonly IConfigurationProvider<EMTSetting> _emtSetting;
 
@@ -53,7 +56,8 @@ namespace emt_sdk.Communication.Discovery
 
         private void Broadcast()
         {
-            _udpClient.Send(_beacon, _beacon.Length);
+            Logger.Log(LogLevel.Debug, $"Broadcasting beacon packet on port {DISCOVERY_PORT}");
+            _udpClient.Send(_beacon, _beacon.Length, _broadcastEndPoint);
         }
 
         public void StartBroadcast()
